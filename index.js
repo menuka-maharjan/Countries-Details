9d3420bc7e98dfcba49765199f44ae2b15fea016
menuka-maharjan <maharjanmenuka8@gmail.com>
2021-01-26 21:48:56 +0530

    adding event listener

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 let countries = " ";
 const Form = document.querySelector("form");
+Form.addEventListener("submit", (event) => {
+    event.preventDefault();
+  
+    countries = document.querySelector("input").value.trim();
+    fetchResult(countries);
+  });
